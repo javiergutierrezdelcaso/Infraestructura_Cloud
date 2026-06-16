4fdae3f7c315fbdc7475a751b8f4e90e2899a326
javiergutierrezdelcaso <javier.gutierrezd@alumnos.viu.es>
2026-06-16 16:26:46 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -183,3 +183,7 @@ resource "azurerm_key_vault_secret" "app_secret" {
 
   depends_on = [azurerm_key_vault.main]
 }
+import {
+  to = azurerm_key_vault_secret.app_secret
+  id = "https://kv-tfg-pre.vault.azure.net/secrets/eco-api-secret"
+}
